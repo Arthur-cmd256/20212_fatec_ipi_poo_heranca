74b16e76c411d2cff88f3126324bc19fe2d56138
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-09-16 14:02:35 -0300

    Classes criada com os atributos e os metodos set e get

---
+++ AlunoDePosGraduacao.java
@@ -0,0 +1,29 @@
+public class AlunoDePosGraduacao {
+    private String nome;
+    private int idade;
+    private char conceito;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setConceito(char conceito) {
+        this.conceito = conceito;
+    }
+
+    public char getConceito() {
+        return conceito;
+    }
+}
---
+++ ProfessorPesquisador.java
@@ -0,0 +1,24 @@
+public class ProfessorPesquisador {
+    private String nome;
+    private int idade;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void pesquisar(){
+        System.out.printf("%s está pesquisando...\n");
+    }
+}
